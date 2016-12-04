4b8694a2ffac0589986e9c99ecae1d5a9bb4d319
Toward <kirillsmolovskii@outlook.com>
2016-12-05 01:32:27 +0300

    Solution with service init

---
+++ CalculatorService/CalculatorService/Services/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using CalculatorService.Contracts;
+
+namespace CalculatorService.Services
+{
+    public class Calculator: ICalculator
+    {
+        public double Add(double a, double b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double Substract(double a, double b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double Multiply(double a, double b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double Divide(double a, double b)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double Sqrt(double a)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
